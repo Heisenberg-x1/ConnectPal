fdaeccc4b4c6b04ff953995921ca6176413f58c0
Byte Me <chemerilcollins@gmail.com>
2023-11-28 15:00:20 +0100

    page under dev

--- src/components/feeds/Feeds.js
+++ src/components/feeds/Feeds.js
@@ -5,6 +5,10 @@ export default function Feeds() {
     <div className="feeds">
       <div className="feedsWrapper">
         <Share />
+        <p>Page under development!</p>
+        <br></br>
+        <p>Check back some other time 😊</p>
+        
       </div>
     </div>
   )
